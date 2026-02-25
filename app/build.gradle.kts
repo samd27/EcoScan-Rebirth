68b375ccba7436fd3245e04d555b326b1abc49e9
samd <sershdiaz@hotmail.com>
2026-02-25 12:47:20 -0600

    Version de la aplicacion ajustada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 15
-        versionName = "1.0"
+        versionName = "15.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
